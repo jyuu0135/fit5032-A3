ac46a265d354a0d7f0cac1aec388eeca5138c2fe
jyuu0135 <jyuu0135@student.monash.edu>
2025-09-09 17:46:33 +1000

    Update: update main.js for pinia function

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-createApp(App).mount('#app')
+import 'bootstrap'
+
+const app = createApp(App)
+app.use(createPinia())
+app.mount('#app')
